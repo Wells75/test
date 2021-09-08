14b68734059c7092007795d2cd35d460973403db
Wells LIN <ht80303@hotmail.com>
2021-09-08 22:04:40 +0200

    add new file

---
+++ py.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as py
+import csv, datetime
+
+# open the file
+f = open("C:/Users/ht803/Desktop/learning/ubike.csv", "r")
+bike = {}
+capacity = {}
+count = {}
+
+# processing the data
+for row in csv.DictReader(f):
+    if row["station"] == "Roosevelt & Xinsheng S. Intersection":
+        time = datetime.datetime.strptime(row["time"], "%Y/%m/%d %H:%M")
+        hour = time.hour
+        if hour not in bike:
+            bike[hour] = int(row["bike"])
+            capacity[hour] = int(row["lot"])
+            count[hour] = 1
+        else:
+            bike[hour] += int(row["bike"])
+            capacity[hour] += int(row["lot"])
+            count[hour] += 1 
+f.close()
+
+# preparing for plotting
+time_seq = bike.keys()
+time_seq = sorted(time_seq)
+avg = []
+lot = []
+for k in time_seq:
+    avg.append(float(bike[k]) / count[k])
+    lot.append(float(capacity[k]) / count[k])
+    
+# plotting the data in avg and lot
+
+
+py.plot(time_seq, lot, label = "Average")
+py.plot(time_seq, avg, label = "Capacity")
+py.title("Bikes at Roosevelt & Xinsheng S. Intersection")
+py.xlabel("Time(hr)")
+py.ylabel("Bike")
+py.legend(loc = "upper right")
+# py.legend()
+# py.legend(location = "upper right")  
+# py.legend(location = "best")    
+
+
+
+# py.ylim(0, max(lot))
+py.ylim(0, max(lot) + 30)  
+py.ylim(0, max(lot) * 1.2)  
+# py.ylimit(0, max(lot) + 30)  
+# py.xticks([0,23]
+py.show()4c
